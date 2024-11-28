bc5098cc3ff18cd212749c871785e6c0944e61c2
Nicholas Larsen <112173371+NLarsen15@users.noreply.github.com>
2024-11-28 13:21:13 +0200

    LiveData Fix 2

--- OTSO/Tool/Parameters/functions/otso_cutoff_bulk.py
+++ OTSO/Tool/Parameters/functions/otso_cutoff_bulk.py
@@ -67,8 +67,9 @@ def OTSO_cutoff_bulk():
             Printtime = round((stop-start),3)
             print("Compuation Took: " + str(Printtime) + " Seconds")
             month, day = misc.day_of_year_to_date(Date[1], Date[0])
+            LiveData = 0
             EventDate = datetime(int(Date[0]),int(month),int(day),int(Date[2]),int(Date[3]),int(Date[4]))
-            readme_generators.READMECutoff(Station_Array, RigidityArray, EventDate, Model, IntModel, AtomicNum, AntiCheck, I, Wind, Magnetopause, File, CoordinateSystem, Printtime, MaxStepPercent*100, EndParams, Rcomp, Rscan)
+            readme_generators.READMECutoff(Station_Array, RigidityArray, EventDate, Model, IntModel, AtomicNum, AntiCheck, I, Wind, Magnetopause, File, CoordinateSystem, Printtime, MaxStepPercent*100, EndParams, Rcomp, Rscan, LiveData)
     elif Bulkcomp == 1:
         for File, Gauss in zip(FileDescriptorsListGauss, GaussCoeffs):
             start = time.time()
@@ -96,7 +97,8 @@ def OTSO_cutoff_bulk():
 
             month, day = misc.day_of_year_to_date(DateArrayGauss[1], DateArrayGauss[0])
             EventDate = datetime(int(DateArrayGauss[0]),int(month),int(day),int(DateArrayGauss[2]),int(DateArrayGauss[3]),int(DateArrayGauss[4]))
-            readme_generators.READMECutoff(Station_Array, RigidityArray, EventDate, Model, IntModel, AtomicNum, AntiCheck, IOPTGauss, WindGauss, Magnetopause, File, CoordinateSystem, Printtime, MaxStepPercent*100, EndParams, Rcomp, Rscan)
+            LiveData = 0
+            readme_generators.READMECutoff(Station_Array, RigidityArray, EventDate, Model, IntModel, AtomicNum, AntiCheck, IOPTGauss, WindGauss, Magnetopause, File, CoordinateSystem, Printtime, MaxStepPercent*100, EndParams, Rcomp, Rscan, LiveData)
     
     finalstop = time.time()
     Printtime = round((finalstop-truestart),3)
